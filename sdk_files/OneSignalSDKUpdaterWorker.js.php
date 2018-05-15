878aa07813a41c05819450f80ce82b577148285e
Maciej Palmowski <m.palmowski@spiders.agency>
2018-05-16 00:33:10 +0200

    Update OneSignalSDKUpdaterWorker.js.php

--- sdk_files/OneSignalSDKUpdaterWorker.js.php
+++ sdk_files/OneSignalSDKUpdaterWorker.js.php
@@ -4,12 +4,15 @@
 	 * Reference: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 	 */
 
+	$onesignal_sdk_debug = apply_filters( 'onesignal_sdk_debug', "importScripts('https://localhost:3001/dev_sdks/OneSignalSDK.js');" );
+	$onesignal_sdk = apply_filters( 'onesignal_sdk', "importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');" );
+
 	header("Service-Worker-Allowed: /");
 	header("Content-Type: application/javascript");
 	header("X-Robots-Tag: none");
 	if (defined('ONESIGNAL_DEBUG') && defined('ONESIGNAL_LOCAL')) {
-		echo "importScripts('https://localhost:3001/dev_sdks/OneSignalSDK.js');";
+		echo $onesignal_sdk_debug;
 	} else {
-		echo "importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');";
+		echo $onesignal_sdk;
 	}
 ?>
