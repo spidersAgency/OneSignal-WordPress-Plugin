0690bd9f871ce4e2f4bc592ac7dbaa5087dfd7b9
Maciej Palmowski <m.palmowski@spiders.agency>
2018-05-16 01:03:19 +0200

    Update OneSignalSDKWorker.js.php

--- sdk_files/OneSignalSDKWorker.js.php
+++ sdk_files/OneSignalSDKWorker.js.php
@@ -3,16 +3,13 @@
 	 * Note: This file is intended to be publicly accessible.
 	 * Reference: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 	 */
-	 
-	$onesignal_sdk_debug = apply_filters( 'onesignal_sdk_debug', "importScripts('https://localhost:3001/dev_sdks/OneSignalSDK.js');" );
-	$onesignal_sdk = apply_filters( 'onesignal_sdk', "importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');" );
-	
 	header("Service-Worker-Allowed: /");
 	header("Content-Type: application/javascript");
 	header("X-Robots-Tag: none");
 	if (defined('ONESIGNAL_DEBUG') && defined('ONESIGNAL_LOCAL')) {
-		echo $onesignal_sdk_debug;
+		echo "importScripts('https://localhost:3001/dev_sdks/OneSignalSDK.js');";
 	} else {
-		echo $onesignal_sdk;
+		echo "importScripts('https://bezprawnik.pl/service-worker.js');";
+		echo "importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');";
 	}
 ?>
